01b727568bf02d39d9fe15e83e1bf95e25225c2b
Elzoka <mahmoud_elzoka@yahoo.com>
2018-11-02 19:51:32 +0200

    initial setup for routes

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 // DB config
 const db = require('./config/keys').mongoURI;
 // connect to mongoDB
@@ -11,6 +15,9 @@ mongoose
     .catch((err) => console.log(err))
 
 app.get('/', (req, res) => res.send('hello'));
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
---
+++ routes/api/profile.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+// @route GET api/profile/test
+// @desc Tests post route
+// @access Public
+
+router.get('/test', (req, res) => res.json({msg: "profile works"}))
+
+
+
+module.exports = router;
