4686d68c3afc488aacfa12209f7f00c538343341
Elzoka <mahmoud_elzoka@yahoo.com>
2018-11-02 19:42:05 +0200

    Connect to mongodb database

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+// DB config
+const db = require('./config/keys').mongoURI;
+// connect to mongoDB
+mongoose
+    .connect(db, {useNewUrlParser: true})
+    .then(() => console.log('MONGODB connected'))
+    .catch((err) => console.log(err))
 
 app.get('/', (req, res) => res.send('hello'));
 
