4b6c7df3c893836c1a3af83970a096313032fade
Elzoka <mahmoud_elzoka@yahoo.com>
2018-11-02 22:29:55 +0200

    create profile model

---
+++ models/Profile.js
@@ -0,0 +1,109 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+// Create Schema
+const ProfileSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    handle: {
+        type: String,
+        required: true,
+        max: 40
+    },
+    company: {
+        type: String
+    },
+    website: {
+        type: String
+    },
+    location: {
+        type: String
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    skills: {
+        type: [String],
+        required: true
+    },
+    bio: {
+        type: Strung
+    },
+    githubUsername: {
+        type: String
+    },
+    experience: [
+        {
+           title: {
+               type: String,
+               required: true
+           },
+           company: {
+               type: String,
+               required: true
+           },
+           location: {
+               type: String,
+           },
+           from: {
+               type: Date,
+               required: true
+           },
+           to: {
+               type: Date,
+           },
+           current: {
+               type: Boolean,
+               default: false
+           },
+           description: {
+               type: String
+           }
+        }
+    ],
+    education: [
+        {
+           school: {
+               type: String,
+               required: true
+           },
+           degree: {
+               type: String,
+               required: true
+           },
+           fieldOfStudy: {
+               type: String,
+           },
+           from: {
+               type: Date,
+               required: true
+           },
+           to: {
+               type: Date,
+           },
+           current: {
+               type: Boolean,
+               default: false
+           },
+           description: {
+               type: String
+           }
+        }
+    ],
+    social: {
+        youtube: String,
+        twitter: String,
+        facebook: String,
+        linkedin: String,
+        instagram: String,
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('profile', ProfileSchema);
