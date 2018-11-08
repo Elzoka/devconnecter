28dcaa17d64ebb8c3628d6df78323ed41a5510be
Elzoka <mahmoud_elzoka@yahoo.com>
2018-11-08 15:14:24 +0200

    add routes for getting profile by handle and id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -32,6 +32,65 @@ router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
         });
 })
 
+// @route  GET api/profile/all
+// @desc   Get all profiles
+// @access Public
+
+router.get('/all', (req, res) => {
+    const errors = {};
+
+    Profile.find({})
+        .populate('user', ['name', 'avatar'])
+        .then(profiles => {
+            if(profiles.length === 0){
+                errors.noprofile = 'There are no profiles';
+                res.status(404).json(errors);
+            }
+
+            res.json(profiles);
+        })
+        .catch(err => res.status(500).json(err));
+});
+
+// @route  GET api/profile/handle/:handle
+// @desc   Get profile by handle
+// @access Public
+
+router.get('/handle/:handle', (req, res) => {
+    const errors = {};
+
+    Profile.findOne({handle: req.params.handle})
+        .populate('user', ['name', 'avatar'])
+        .then(profile => {
+            if(!profile){
+                errors.noprofile = 'There\'s no profile for this user';
+                res.status(404).json(errors);
+            }
+
+            res.json(profile);
+        })
+        .catch(err => res.status(500).json(err));
+});
+
+// @route  GET api/profile/user/:user_id
+// @desc   Get profile by user ID
+// @access Public
+
+router.get('/user/:user_id', (req, res) => {
+    const errors = {};
+
+    Profile.findOne({user: req.params.user_id})
+        .populate('user', ['name', 'avatar'])
+        .then(profile => {
+            if(!profile){
+                errors.noprofile = 'There\'s no profile for this user';
+                res.status(404).json(errors);
+            }
+
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json({profile: 'There\'s no profile for this user'}));
+})
 
 // @route  POST api/profile
 // @desc   Create user or edit profile
