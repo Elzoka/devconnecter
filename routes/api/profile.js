24008e1690f1ccf4f678617631d8657ec1fc44b2
Elzoka <mahmoud_elzoka@yahoo.com>
2018-11-08 14:35:16 +0200

    validate profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,8 @@ const router = express.Router();
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
+const validateProfileInput = require('../../validation/profile');
+
 // @route GET api/profile/test
 // @desc Tests post route
 // @access Public
@@ -19,6 +21,7 @@ router.get('/test', (req, res) => res.json({msg: "profile works"}))
 router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
     const errors = {};
     Profile.findOne({user: req.user.id})
+        .populate('user', ['name', 'avatar'])
         .then(profile => {
             errors.noprofile = 'There is no profile for this user';
             if(!profile) return res.status(404).json(errors);
@@ -29,15 +32,22 @@ router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
         });
 })
 
+
 // @route  POST api/profile
 // @desc   Create user or edit profile
 // @access private
 
 router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
-    const errors = {};
+    const {errors, isValid} = validateProfileInput(req.body);
+
+    if(!isValid){
+        return res.status(400).json(errors);
+    }
+
     const profileFields = {};
     profileFields.user = req.user.id;
     if(req.body.handle) profileFields.handle = req.body.handle;
+    if(req.body.status) profileFields.status = req.body.status;
     if(req.body.company) profileFields.company = req.body.company;
     if(req.body.website) profileFields.website = req.body.website;
     if(req.body.location) profileFields.location = req.body.location;
